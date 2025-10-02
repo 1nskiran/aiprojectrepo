9f7b6803aef15055ed420111be9741d5df421d8e
n_saikiran2003@yahoo.co.in <n_saikiran2003@yahoo.co.in>
2025-10-02 13:40:15 +0530

    test

--- openai-gemini-integration.py
+++ openai-gemini-integration.py
@@ -6,7 +6,7 @@ load_dotenv()
 client=OpenAI(base_url="https://generativelanguage.googleapis.com/v1beta/")
 
 response=client.chat.completions.create(model="gemini-2.5-flash",
-                                        messages=[{"role":"user",
-                                                   "content":"I am x! Nice to meet you. Who are you?"}])
+                                        messages=[{"role":"system","content":"You are an expert in Quantum Computing and you should focus on topics relted to Quantum Computing only. If you need to answer questions not pertaining to Quantum Computing, just say sorry and you do not wish to go out of context"},
+                                            {"role":"user","content":"What's the role of python programming language data analytics?"}])
 
 print(response.choices[0].message.content)
