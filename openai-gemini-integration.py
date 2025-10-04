2e1ebb638add1c2ebbf85a76f435995374fdb0b3
n_saikiran2003@yahoo.co.in <n_saikiran2003@yahoo.co.in>
2025-10-04 20:01:02 +0530

    added zero,few shots and cot changes

--- openai-gemini-integration.py
+++ openai-gemini-integration.py
@@ -5,8 +5,12 @@ load_dotenv()
 
 client=OpenAI(base_url="https://generativelanguage.googleapis.com/v1beta/")
 
+#zero shot
+SYSTEM_PROMPT="You should answer questions related to human body. Otherthan this you should say sorry"
+
 response=client.chat.completions.create(model="gemini-2.5-flash",
-                                        messages=[{"role":"system","content":"You are an expert in Quantum Computing and you should focus on topics relted to Quantum Computing only. If you need to answer questions not pertaining to Quantum Computing, just say sorry and you do not wish to go out of context"},
-                                            {"role":"user","content":"What's the role of python programming language data analytics?"}])
+                                        messages=[{"role":"system","content":SYSTEM_PROMPT},
+                                                  {"role":"user","content":"what is a computer?"}
+                                                ])
 
 print(response.choices[0].message.content)
